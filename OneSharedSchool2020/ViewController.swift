feea6da068b4c22c6ddfb0833f247a562c6a71b2
AshwinRajesh <ashwinr2003@yahoo.com>
2020-01-25 11:26:11 -0800

    Initial Commit

---
+++ OneSharedSchool2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneSharedSchool2020
+//
+//  Created by Ashwin Rajesh on 1/25/20.
+//  Copyright © 2020 TinovationiOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
